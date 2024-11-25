d941ef36858a81a94addf7ed69eab8c593446a82
Brett Fuller <bmfuller81@gmail.com>
2024-11-24 20:12:06 -0600

    added module 6 and assignments

--- module-6/forestfiresim_325.py
+++ module-6/forestfiresim_325.py
@@ -1,10 +1,18 @@
+#Cindy Hernandez
+#Brett Fuller
+#Nardos Gebremedhin
+#Elizabeth Hinz
+#11/24/2024
+#CSD 325  – Assignment 6.2
+
 """Forest Fire Sim, modified by Sue Sampson, based on a program by Al Sweigart
 A simulation of wildfires spreading in a forest. Press Ctrl-C to stop.
 Inspired by Nicky Case's Emoji Sim http://ncase.me/simulating/model/
 ** use spaces, not indentation to modify **
 Tags: short, bext, simulation"""
 
-import random, sys, time, math
+
+import random, sys, time
 
 try:
     import bext
@@ -21,7 +29,7 @@ HEIGHT = 22
 TREE = 'A'
 FIRE = '@'
 EMPTY = ' '
-LAKE = '~'
+LAKE = '~'  # New symbol for lake
 
 # (!) Try changing these settings to anything between 0.0 and 1.0:
 INITIAL_TREE_DENSITY = 0.20  # Amount of forest that starts with trees.
@@ -31,13 +39,9 @@ FIRE_CHANCE = 0.01  # Chance a tree is hit by lightning & burns.
 # (!) Try setting the pause length to 1.0 or 0.0:
 PAUSE_LENGTH = 0.5
 
-WATER_BLOCKS_LINE_OF_SITE = True
-
 def main():
-    forest = createNewForest()
-    forest = addLakeToForest(forest)
+    forest = createnewForestWithLake()
     bext.clear()
-    
 
     while True:  # Main program loop.
         displayForest(forest)
@@ -49,8 +53,6 @@ def main():
         for x in range(forest['width']):
             for y in range(forest['height']):
                 if (x, y) in nextForest:
-                    # If we've already set nextForest[(x, y)] on a
-                    # previous iteration, just do nothing here:
                     continue
 
                 if ((forest[(x, y)] == EMPTY)
@@ -62,82 +64,52 @@ def main():
                     # Lightning sets this tree on fire.
                     nextForest[(x, y)] = FIRE
                 elif forest[(x, y)] == FIRE:
-                    # This tree is currently burning.
-                    # Loop through all the neighboring spaces:
-                    #set default range for checking firespread
-                    xRxLow = -1
-                    xRxHigh = 2
-                    yRyLow = -1
-                    yRyHigh = 2
-                    #if we decide diagonals are blocked by lakes override the above values if needed
-                    if WATER_BLOCKS_LINE_OF_SITE == True:
-                        if forest.get((x - 1, y)) == LAKE:
-                            xRxLow = 0
-                        if forest.get((x + 1, y)) == LAKE:
-                            xRxHigh = 1
-                        if forest.get((x, y-1)) == LAKE:
-                            yRyLow = 0
-                        if forest.get((x, y+1)) == LAKE:
-                            yRyHigh = 1
-
-                    for ix in range(xRxLow, xRxHigh):
-                        for iy in range(yRyLow, yRyHigh):
-                            # Fire spreads to neighboring trees:
+                    # Fire spreads to neighboring trees (unless there's a lake):
+                    for ix in range(-1, 2):
+                        for iy in range(-1, 2):
                             if forest.get((x + ix, y + iy)) == TREE:
-                                nextForest[(x + ix, y + iy)] = FIRE
+                                if forest.get((x + ix, y + iy)) != LAKE:
+                                    nextForest[(x + ix, y + iy)] = FIRE
                     # The tree has burned down now, so erase it:
                     nextForest[(x, y)] = EMPTY
                 else:
-                    # Just copy the existing object: - This also accounts for the lake
+                    # Just copy the existing object:
                     nextForest[(x, y)] = forest[(x, y)]
         forest = nextForest
 
         time.sleep(PAUSE_LENGTH)
 
-#function to create a randomly sized length in the middle of the map
-def addLakeToForest(forest):
-    if WIDTH > 14:
-        lakeWidth = random.randint(1, 12)
-    elif WIDTH > 2:
-        lakeWidth = random.randint(1, (WIDTH - 2))
-    else:
-        lakeWidth = 1
-    if HEIGHT > 14:
-        lakeHeight = random.randint(1, 12)
-    elif HEIGHT > 2:
-        lakeHeight = random.randint(1, (HEIGHT - 2))
-    else:
-        lakeHeight = 1
-    
-    xStart = math.floor(WIDTH / 2) - math.floor(lakeWidth / 2)
-    if(xStart <= 0 ):
-        xStart = 1
-    xEnd = xStart + lakeWidth
 
-    yStart = math.floor(HEIGHT / 2) - math.floor(lakeHeight / 2)
-    if(yStart <= 0):
-        yStart = 1
-    yEnd = yStart + lakeHeight
-    
-    for x in range(xStart, xEnd):
-        for y in range(yStart, yEnd):
-            forest[(x,y)] = LAKE
-    
-    return forest
-
-
-
-def createNewForest():
-    """Returns a dictionary for a new forest data structure."""
+def createnewForestWithLake():
+    """Returns a dictionary for a new forest data structure with a lake."""
     forest = {'width': WIDTH, 'height': HEIGHT}
+    
+    # Initialize the forest with trees and empty spaces
     for x in range(WIDTH):
         for y in range(HEIGHT):
             if (random.random() * 100) <= INITIAL_TREE_DENSITY:
                 forest[(x, y)] = TREE  # Start as a tree.
             else:
                 forest[(x, y)] = EMPTY  # Start as an empty space.
+
+    # Add the lake to the forest
+    addLakeToForest(forest)
     return forest
 
+def addLakeToForest(forest):
+    """Add a rectangular lake to the forest, centered in the middle."""
+    # Fixed lake size defined inside this function
+    lake_width = 10  # You can change this to any fixed width
+    lake_height = 5  # You can change this to any fixed height
+
+    # Center the lake in the forest
+    lake_x = int((WIDTH / 2) - (lake_width / 2))  # Centered X position
+    lake_y = int((HEIGHT / 2) - (lake_height / 2))  # Centered Y position
+
+    # Fill the area with the lake symbol (~)
+    for x in range(lake_x, lake_x + lake_width):
+        for y in range(lake_y, lake_y + lake_height):
+            forest[(x, y)] = LAKE  # Mark as lake
 
 def displayForest(forest):
     """Display the forest data structure on the screen."""
@@ -151,16 +123,15 @@ def displayForest(forest):
                 bext.fg('red')
                 print(FIRE, end='')
             elif forest[(x, y)] == LAKE:
-                bext.fg('blue')
-                print(LAKE, end='')            	
+                bext.fg('blue')     
+                print(LAKE, end='')  # Display lake '~' in blue
             elif forest[(x, y)] == EMPTY:
-                print(EMPTY, end='')          
+                print(EMPTY, end='')
         print()
-    bext.fg('reset')  # Use the default font color.
+    bext.fg('reset')  # default font color.
     print('Grow chance: {}%  '.format(GROW_CHANCE * 100), end='')
     print('Lightning chance: {}%  '.format(FIRE_CHANCE * 100), end='')
     print('Press Ctrl-C to quit.')
-
 
 # If this program was run (instead of imported), run the game:
 if __name__ == '__main__':
